9a3593519eaa4bab1004c045c9ae489924f48895
Lisa Stedman-Falls <lstedmanfalls@gmail.com>
2021-09-08 15:33:09 -0600

    Added 'length' SLL algo

---
+++ Javascript_Algos/Length.js
@@ -0,0 +1,37 @@
+// Create a new SLL method length() that returns number of nodes in that list.
+
+class SLLNode {
+    constructor(data){
+        this.value = data;
+        this.next = null;
+    }
+}
+
+class SLL{
+    constructor(){
+        this.head = null;
+    }
+    addNode(value){
+        var NewNode = new SLLNode(value);
+        NewNode.next = this.head;
+        this.head = NewNode;
+        return this;
+    }
+    length(val){
+        var count = 0;
+        var runner = this.head;
+        while (runner != null){
+            if (runner.value == val){
+                return true;
+            }
+            runner = runner.next;
+            count +=1
+        }
+        return count;
+    }
+}
+
+var newSLL = new SLL()
+newSLL.addNode(-1).addNode(2).addNode(3).addNode(4).addNode(10)
+// console.log(newSLL)
+console.log(newSLL.length())
