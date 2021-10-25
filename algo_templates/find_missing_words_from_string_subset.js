6389774caa7b28f02029e0e5ef04be217b73ac30
Lisa Stedman-Falls <lstedmanfalls@gmail.com>
2021-10-25 12:46:53 -0600

    refactored find_missing_words

--- algo_templates/find_missing_words_from_string_subset.js
+++ algo_templates/find_missing_words_from_string_subset.js
@@ -1,10 +1,10 @@
 // Given a string, and another string that is a subset of the first string, find the words that are missing in the second string from the first string.
 
-function stringDiff (s, t){
-    var arr = [];
-    var str1 = s.split(" ");
-    var str2 = t.split(" ");
-    for (var i = 0; i < str1.length; i++){
+const stringDiff = (s, t) => {
+    const arr = [];
+    const str1 = s.split(" ");
+    const str2 = t.split(" ");
+    for (let i = 0; i < str1.length; i++){
         if (! (str2.includes(str1[i]))){
             arr.push(str1[i])
         }
