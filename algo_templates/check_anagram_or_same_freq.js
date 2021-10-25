2851308ade324383fe34515fca488ec5f0cab3b5
Lisa Stedman-Falls <lstedmanfalls@gmail.com>
2021-10-25 12:49:09 -0600

    Refactored check_anagram

--- algo_templates/check_anagram_or_same_freq.js
+++ algo_templates/check_anagram_or_same_freq.js
@@ -1,11 +1,11 @@
 // Given two arrays, determine whether the second array is an anagram of the first (words reordered in different sequence). Return true or false
 
-function anagram(str1, str2){
+const anagram = (str1, str2) => {
     if (str1.length !== str2.length){ // If the lengths of the strings are different, they are immediately not anagrams
         return false;
     }
-    let obj1 = {} // Set up empty objects to count frequency of each string character for each of the 2 strings
-    let obj2 = {}
+    const obj1 = {} // Set up empty objects to count frequency of each string character for each of the 2 strings
+    const obj2 = {}
     for (let val of str1){ // of checks for values of arrays, etc.
         obj1[val] = ++obj1[val] || 0; // Frequency count for each string
     }
