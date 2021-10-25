581234ebc4603d3bf5f9b806e281cdc7a2fd03d7
Lisa Stedman-Falls <lstedmanfalls@gmail.com>
2021-10-25 12:36:17 -0600

    refactored templates

--- algo_templates/check_anagram_or_same_freq.js
+++ algo_templates/check_anagram_or_same_freq.js
@@ -6,13 +6,13 @@ function anagram(str1, str2){
     }
     let obj1 = {} // Set up empty objects to count frequency of each string character for each of the 2 strings
     let obj2 = {}
-    for (let val of str1){ // of checks for values of the keys
+    for (let val of str1){ // of checks for values of arrays, etc.
         obj1[val] = ++obj1[val] || 0; // Frequency count for each string
     }
     for (let val of str2){
         obj2[val] = ++obj2[val] || 0;
     }
-    for (let key in obj1){ // in checks for keys in the objects
+    for (let key in obj1){ // in checks for keys in objects
         if (!(key in obj2)){ // if the key isn't there, they're not anagrams (must include the same letters)
             return false
         }
@@ -29,3 +29,35 @@ console.log(anagram("rat", "car")) // false
 console.log(anagram("awesome", "awesom")) // false
 console.log(anagram("qwerty", "qeywrt")) // true
 console.log(anagram("texttwisttime", "timetexttwist")) // true
+
+
+// Given two positive integers, deteremine if they have the same frequency of digits.
+const sameFrequency = (num1, num2) => {
+    const str1 = num1.toString()
+    const str2 = num2.toString()
+    if (str1.length !== str2.length){
+        return false
+    }
+    let obj1 = {}
+    let obj2 = {}
+    for (let val of str1){
+        obj1[val] = ++obj1[val] || 0;
+    }
+    for (let val of str2){
+        obj2[val] = ++obj2[val] || 0;
+    }
+    for (let key in obj1) {
+        if (!(key in obj2)){
+            return false
+        }
+        if(obj1[key] !== obj2[key]){
+            return false
+        }
+    }
+    return true
+}
+
+console.log(sameFrequency(182,281)) //true
+console.log(sameFrequency(34,14)) //false
+console.log(sameFrequency(3589578,5879385)) //true
+console.log(sameFrequency(22,222)) //false
---
+++ algo_templates/check_duplicates_variable_number_of_arguments.js
@@ -0,0 +1,17 @@
+function areThereDuplicates(...nums) {
+    const allNums = nums;
+    const obj = {};
+    if (allNums.length > 1){
+        for (let val of allNums){
+            obj[val] = ++obj[val] || 0;
+            if (obj[val] >=1){
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+console.log(areThereDuplicates(1,2,2,4))
+console.log(areThereDuplicates(0))
+console.log(areThereDuplicates(0,0))
